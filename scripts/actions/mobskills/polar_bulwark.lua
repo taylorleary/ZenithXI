ecb6577ed763e730bb3e47205bd3ec8341a565fe
Will Buck <wccbuck@gmail.com>
2026-01-17 15:26:19 -0600

    Fix Nerve Gas ID for Tinnin Bulwarks

--- scripts/actions/mobskills/polar_bulwark.lua
+++ scripts/actions/mobskills/polar_bulwark.lua
@@ -20,7 +20,7 @@ mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     mob:addStatusEffectEx(xi.effect.MAGIC_SHIELD, 0, 1, 0, 45)
     skill:setMsg(xi.msg.basic.SKILL_GAIN_EFFECT)
     if mob:getFamily() == 313 then -- Tinnin follows this up immediately with Nerve Gas
-        mob:useMobAbility(1580)
+        mob:useMobAbility(1836)
     end
 
     return xi.effect.MAGIC_SHIELD
