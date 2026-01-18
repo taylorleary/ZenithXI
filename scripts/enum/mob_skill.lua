87cf3a01ad7b3b676730b00fe41d7cf0b962a6bc
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-01-18 14:08:11 -0500

    [lua] [sql] ENM Shell We Dance?
    Implements the ENM Shell We Dance?

--- scripts/enum/mob_skill.lua
+++ scripts/enum/mob_skill.lua
@@ -162,6 +162,11 @@ xi.mobSkill =
     FRIGHTFUL_ROAR_1              =  501,
     MORTAL_RAY_1                  =  502,
     UNBLESSED_ARMOR               =  503,
+    GAS_SHELL_1                   =  504,
+    VENOM_SHELL_1                 =  505,
+    PALSYNYXIS_1                  =  506,
+    PAINFUL_WHIP_1                =  507,
+    SUCTORIAL_TENTACLE_1          =  508,
 
     SELF_DESTRUCT_BOMB            =  509,
 
