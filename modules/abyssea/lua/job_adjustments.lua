df5a235250821db2b19cb9db90d18d5038f2b8b6
Critical <48370698+CriticalXI@users.noreply.github.com>
2026-01-28 11:24:42 -0700

    [lua, sql] White Mage Era Audits

--- modules/abyssea/lua/job_adjustments.lua
+++ modules/abyssea/lua/job_adjustments.lua
@@ -13,12 +13,12 @@ local m = Module:new('job_adjustments')
 -- Warrior
 -----------------------------------
 
--- Remove Triple Attack from Warrior's Charge
+-- Warrior's Charge: Remove Triple Attack bonus
 m:addOverride('xi.effects.warriors_charge.onEffectGain', function(target, effect)
     effect:addMod(xi.mod.DOUBLE_ATTACK, 100)
 end)
 
--- Set Warrior's Charge recast to be reduced by merits
+-- Warrior's Charge: Apply merit recast reduction
 m:addOverride('xi.job_utils.warrior.useWarriorsCharge', function(player, target, ability, action)
     local recastReduction = player:getMerit(xi.merit.WARRIORS_CHARGE) - 150
     action:setRecast(action:getRecast() - recastReduction)
@@ -26,6 +26,44 @@ m:addOverride('xi.job_utils.warrior.useWarriorsCharge', function(player, target,
     player:addStatusEffect(xi.effect.WARRIORS_CHARGE, 1, 0, 60)
 
     return xi.effect.WARRIORS_CHARGE
+end)
+
+-----------------------------------
+-- White Mage
+-----------------------------------
+
+-- Martyr: Apply merit recast reduction, remove merit healing bonus
+m:addOverride('xi.job_utils.white_mage.useMartyr', function(player, target, ability, action)
+    local recastReduction = player:getMerit(xi.merit.MARTYR) - 150
+    action:setRecast(action:getRecast() - recastReduction)
+
+    local damageHP = math.floor(player:getHP() * 0.25)
+    local healHP = damageHP * 2
+    healHP = utils.clamp(healHP, 0, target:getMaxHP() - target:getHP())
+
+    -- If stoneskin is present, it should absorb damage
+    damageHP = utils.handleStoneskin(player, damageHP)
+    player:delHP(damageHP)
+    target:addHP(healHP)
+
+    return healHP
+end)
+
+-- Devotion: Apply merit recast reduction, remove merit MP bonus
+m:addOverride('xi.job_utils.white_mage.useDevotion', function(player, target, ability, action)
+    local recastReduction = player:getMerit(xi.merit.DEVOTION) - 150
+    action:setRecast(action:getRecast() - recastReduction)
+
+    local damageHP = math.floor(player:getHP() * 0.25)
+    local healMP = damageHP
+    healMP = utils.clamp(healMP, 0, target:getMaxMP() - target:getMP())
+
+    -- If stoneskin is present, it should absorb damage
+    damageHP = utils.handleStoneskin(player, damageHP)
+    player:delHP(damageHP)
+    target:addMP(healMP)
+
+    return healMP
 end)
 
 return m
--- modules/abyssea/sql/job_adjustments.sql
+++ modules/abyssea/sql/job_adjustments.sql
@@ -1,16 +1,54 @@
 ------------------------------------
 -- Abyssea Job SQL Adjustments
 -- This module reverts relevant SQL tables for jobs to their pre-Abyssea values
-------------------------------------
--- Source : https://www.bg-wiki.com/ffxi/Version_Update_(03/26/2012)
+-- Unless otherwise noted, all changes here are sourced from: https://www.bg-wiki.com/ffxi/Version_Update_(03/26/2012)
 ------------------------------------
 
 ------------------------------------
 -- Warrior
 ------------------------------------
 
--- Reverts Warrior's Charge cooldown to 15 minutes
+-- Warrior's Charge: Revert recast from 5 to 15 minutes
 UPDATE abilities SET recastTime = 900 WHERE name = 'warriors_charge';
 
--- Change warrior's Charge merit value to reduce cooldown by 150 seconds per merit
+-- Warrior's Charge merit: Revert value to 150 seconds per level
 UPDATE merits SET value = 150 WHERE name = 'warriors_charge';
+
+------------------------------------
+-- White Mage
+------------------------------------
+
+-- Martyr: Revert recast from 10 to 20 minutes
+UPDATE abilities SET recastTime = 1200 WHERE name = 'martyr';
+
+-- Devotion: Revert recast from 10 to 20 minutes
+UPDATE abilities SET recastTime = 1200 WHERE name = 'devotion';
+
+-- Martyr merit: Revert value to 150 seconds per level
+UPDATE merits SET value = 150 WHERE name = 'martyr';
+
+-- Devotion merit: Revert value to 150 seconds per level
+UPDATE merits SET value = 150 WHERE name = 'devotion';
+
+-- Animus Solace: Disable merit upgrades
+UPDATE merits SET upgrade = 0 WHERE name = 'animus_solace';
+
+-- Animus Misery: Disable merit upgrades
+UPDATE merits SET upgrade = 0 WHERE name = 'animus_misery';
+
+------------------------------------
+-- WHM Spell Cast Times
+-- Source: https://www.bg-wiki.com/ffxi/Version_Update_(02/13/2012)
+------------------------------------
+
+-- Esuna: Revert cast time from 1 to 3 seconds
+UPDATE spell_list SET castTime = 3000 WHERE name = 'esuna';
+
+-- Sacrifice: Revert cast time from 1 to 1.5 seconds
+UPDATE spell_list SET castTime = 1500 WHERE name = 'sacrifice';
+
+-- Blindna: Revert cast time from 1 to 3 seconds
+UPDATE spell_list SET castTime = 3000 WHERE name = 'blindna';
+
+-- Cursna: Revert cast time from 1 to 3 seconds
+UPDATE spell_list SET castTime = 3000 WHERE name = 'cursna';
