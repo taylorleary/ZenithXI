976f41325fda31eaf0e65c7a343abcfbdec48cd5
MrSent <mrsentientone@gmail.com>
2026-02-23 20:28:47 +0000

    convert chronoshift to use copyStatusEffect

--- scripts/actions/abilities/pets/chronoshift.lua
+++ scripts/actions/abilities/pets/chronoshift.lua
@@ -14,20 +14,15 @@ abilityObject.onPetAbility = function(target, pet, petskill, summoner, action)
     local effect      = pet:getStatusEffect(effectID) or nil -- Find that effect on Atomos
 
     if effect then
-        local effectIcon  = effect:getIcon()
-        local power       = effect:getPower()
-        local duration    = effect:getDuration() / 1000
         -- Delete old effect if on target already
         target:delStatusEffectSilent(effectID)
         -- Add the stolen effect to the party
-        target:addStatusEffectEx(effectID, effectIcon, power, 0, duration, true)
+        target:copyStatusEffect(effect)
         petskill:setMsg(xi.msg.basic.RECEIVE_MAGICAL_EFFECT)
         effectCount = 1
     else
         petskill:setMsg(xi.msg.basic.NO_EFFECT)
     end
-
-    target:addEnmity(pet, 1, 60)
 
     pet:timer(5000, function()
         if summoner then
